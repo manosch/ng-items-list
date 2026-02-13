fc88bd5d85ebc47add69273d6221ff3dd74c023e
Manos Chatzigavriil <manos.chatzigavriil@gmail.com>
2026-02-13 12:57:35 +0200

    feat(characters): add characters store base methods/state

---
+++ src/app/state/characters-store/characters-store.ts
@@ -0,0 +1,53 @@
+
+import { patchState, signalStore, withComputed, withHooks, withMethods, withProps, withState } from '@ngrx/signals';
+import { computed } from '@angular/core';
+import { inject } from '@angular/core';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { pipe, switchMap, filter, tap } from 'rxjs';
+import { CharDTO } from '../../api/models/response-dto';
+import { CharacterApi } from '../../api/services/character-api.ts';
+
+
+type CharactersState = {
+  characters: CharDTO[];
+  currentPage: number;
+  loading: boolean;
+}
+
+const initialState: CharactersState = {
+  characters: [],
+  currentPage: 1,
+  loading: false
+}
+
+export const CharactersStore = signalStore(
+  withState(initialState),
+  withProps(() => ({
+    api: inject(CharacterApi)
+  })),
+  withMethods(({ api, ...store }) => ({
+    fetchCharacters: rxMethod(
+      pipe(
+        switchMap(() => {
+          patchState(store, { loading: true })
+          return api.fetchData(store.currentPage()).pipe(
+            tap(response => {
+              patchState(store, {
+                characters: [...store.characters(), ...response.results],
+                currentPage: store.currentPage() + 1,
+                loading: false
+              })
+            })
+          )
+        })
+      )
+    ),
+    setCurrentPage: (page: number) => {
+      patchState(store, { currentPage: page });
+    },
+    nextPage: () => {
+      const current = store.currentPage();
+      patchState(store, { currentPage: current + 1 });
+    },
+  }))
+)
