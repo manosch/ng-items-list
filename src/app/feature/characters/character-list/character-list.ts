485809f058eb61479f3dfbd223b5637d5daf8967
Manos Chatzigavriil <manos.chatzigavriil@gmail.com>
2026-02-13 12:59:53 +0200

    feat(characters): implement characters list layout and initial page loading

--- src/app/feature/characters/character-list/character-list.ts
+++ src/app/feature/characters/character-list/character-list.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CharactersFacade } from '../characters-facade.ts';
+import { CharactersStore } from '../../../state/characters-store/characters-store.js';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { CharacterCard } from '../../../shared/components/character-card/character-card.js';
 
 @Component({
   selector: 'app-character-list',
-  imports: [],
+  imports: [MatGridListModule, CharacterCard],
   templateUrl: './character-list.html',
   styleUrl: './character-list.scss',
+  providers: [CharactersFacade, CharactersStore]
 })
 export class CharacterList {
+  charactersFacade = inject(CharactersFacade);
 
+  characters = this.charactersFacade.charactersList;
+  loading = this.charactersFacade.loading;
+
+  constructor() {
+    this.charactersFacade.loadCharactersPage(1);
+  }
 }
