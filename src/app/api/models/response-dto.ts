b33de48379170678cc77a2401ed6e53fab6e199d
Manos Chatzigavriil <manos.chatzigavriil@gmail.com>
2026-02-13 15:05:35 +0200

    feat(characters): update character api layer to support extra info

--- src/app/api/models/response-dto.ts
+++ src/app/api/models/response-dto.ts
@@ -1,17 +1,29 @@
+export interface Location {
+  name: string;
+  url: string;
+}
+
 export interface CharDTO {
   id: number;
   name: string;
-  status: "Dead" | "Alive";
+  status: "Dead" | "Alive" | "unknown";
   species: string;
+  type: string;
+  gender: "Female" | "Male" | "Genderless" | "unknown";
+  origin: Location;
+  location: Location;
   image: string;
-
+  episode: string[];
+  url: string;
+  created: string;
 }
 
 export interface ResponseDTO {
   info: {
     count: number;
     pages: number;
-    prev: number | null
+    prev: number | null;
+    next: string | null;
   }
-  results: CharDTO[]
+  results: CharDTO[];
 }
--- src/app/api/services/character-api.ts.ts
+++ src/app/api/services/character-api.ts.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ResponseDTO } from '../models/response-dto';
+import { CharDTO, ResponseDTO } from '../models/response-dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterApi {
-  http = inject(HttpClient)
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = 'https://rickandmortyapi.com/api/character';
 
-  fetchData(page: number): Observable<ResponseDTO> {
-    return this.http.get<ResponseDTO>('https://rickandmortyapi.com/api/character', { params: { page } })
+  fetchCharacters(page: number): Observable<ResponseDTO> {
+    return this.http.get<ResponseDTO>(this.baseUrl, { params: { page } });
+  }
+
+  getCharacter(id: number): Observable<CharDTO> {
+    return this.http.get<CharDTO>(`${this.baseUrl}/${id}`);
   }
 }
